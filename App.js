eb0349a18b034f4cc8db2eb308b2a328cf28bda9
Kohei TAKATA <kt.koheitakata@gmail.com>
2017-09-16 16:55:35 +0900

    Create TextInput

--- App.js
+++ App.js
@@ -4,47 +4,44 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    TextInput,
+    ScrollView,
+    View
 } from 'react-native';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {text: 'Welcome to React Native!'};
+    }
+
+    render() {
+        return (
+            <ScrollView
+                contentContainerStyle={styles.container}>
+                <TextInput
+                    multiline={true}
+                    style={styles.welcome}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}/>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flexGrow: 1,
+        justifyContent: 'center', flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        backgroundColor: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
 });
